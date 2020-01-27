be88636bb94fe248b54e8b8e6a3c943c09dee711
Mik Medina <mik@djangoforce.com>
2020-01-27 22:03:37 +0800

    API: Profile picture

--- mobile/components/base/CustomHeader.js
+++ mobile/components/base/CustomHeader.js
@@ -59,13 +59,14 @@ export default class StudentHomeScreen extends React.Component {
             }}
           >
             {
-              this.props.profilePicture ?
-                <Thumbnail
+              this.props.profilePicture
+              ? <Thumbnail
+                  source={{ uri: this.props.profilePicture }}
+                />
+              : <Thumbnail
                   source={{ uri: 'https://66.media.tumblr.com/090f255775a02ea78c588b9c6c9da5e8/tumblr_pj2d3d1F9i1uym0tp_540.jpg' }}
-                /> :
-                <Thumbnail
-                  source={{ uri: 'https://66.media.tumblr.com/090f255775a02ea78c588b9c6c9da5e8/tumblr_pj2d3d1F9i1uym0tp_540.jpg' }}
-                  />
+                />
+
             }
             <View style={{ padding: 10, flexDirection: 'column' }}>
               <Text style={{ textAlign: 'center', letterSpacing: 5, color: Colors.white, backgroundColor: Colors.tint, padding: .5 }}>
